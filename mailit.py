c1a682362bd549a76b34c09e774d78737014ba89
Rajnish Singh <78335498+rajnish-488@users.noreply.github.com>
2021-10-26 23:04:44 +0530

    the bulk email backend

---
+++ mailit.py
@@ -0,0 +1,45 @@
+import smtplib
+import os
+from email.message import EmailMessage
+import pathlib
+
+# globel variable which we will be using
+reciverEmials = "rajnishsingh04012001@gmail.com, 488_bt19@iiitkalyani.ac.in" # the list of the recivers (comma seperated email addresses)
+senderEmail="rajnishsingh1080@gmail.com" #email of the senders
+senderPassword="Rajnish@123" #email password od the sender
+emailSubject=" the thin is a subject"
+messageInText="done it by new methord"
+emailHtmlPath="index.html"
+emailAttachmentPath="C:/Users/hp/Desktop/classwork/labass7_8051_488.pdf"
+emailCode="233#$dfr$%5"
+
+
+
+def send_email():
+    message=EmailMessage()
+    # to add details.
+    message["Subject"]=emailSubject
+    message["From"]=senderEmail
+    message["To"]=reciverEmials
+
+    # to add text
+    message.set_content(messageInText)
+
+    # to add html file
+    html_message=open(emailHtmlPath).read()
+    message.add_alternative(html_message,subtype='html')
+
+    # to add the attachments
+    with open(emailAttachmentPath,"rb") as file:
+        file_name=file.name
+        file_type=pathlib.Path(emailAttachmentPath).suffix # to get the file extenction
+        file_data=file.read()
+    message.add_attachment(file_data,maintype="application",subtype=file_type,filename=file_name)
+
+
+    with smtplib.SMTP_SSL("smtp.gmail.com",465) as smtp:
+        smtp.login(senderEmail,senderPassword)
+        smtp.send_message(message)
+    print("message send")
+
+send_email()
